4bbb4ba11a6f633b4a86c1a8ddff8f4663faee10
MajoritySky <gzag2496@gmail.com>
2022-11-24 14:25:51 +0500

    commit 3.0

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val search_button = findViewById<Button>(R.id.search_button)
+        val mediateca_button = findViewById<Button>(R.id.mediateca_button)
+        val setting_button = findViewById<Button>(R.id.settings_button)
+
+        search_button.setOnClickListener{
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+        mediateca_button.setOnClickListener{
+            val displayIntent = Intent(this, MediatecaActivity::class.java)
+            startActivity(displayIntent)
+        }
+        setting_button.setOnClickListener{
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
+        }
     }
 }
