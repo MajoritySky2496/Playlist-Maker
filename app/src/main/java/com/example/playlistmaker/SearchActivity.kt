f26b3115b967d8f070b6510f7362b1c7a6fbdfba
MajoritySky2496 <gzag2496@gmail.com>
2022-12-10 17:02:27 +0500

    My Third Commit

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,82 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 
 class SearchActivity : AppCompatActivity() {
+
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val backButton = findViewById<ImageView>(R.id.back_button)
+
+        if (savedInstanceState != null){
+            val text = savedInstanceState.getString(PRODUCT_AMOUNT)
+            if (!text.isNullOrEmpty()){
+                inputEditText.setText(text)
+            }
+        }
+        clearButton.setOnClickListener{
+            inputEditText.setText("")
+            hideKeyboard(currentFocus ?: View(this))
+        }
+        backButton.setOnClickListener{
+            finish()
+        }
+        val simpleTextWatcher = object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                clearButton.visibility = clearButtonVisibility(p0)
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        }
+
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
     }
+    private fun  clearButtonVisibility(p0: CharSequence?): Int{
+        return  if (p0.isNullOrEmpty()){
+            View.GONE
+        }else{
+           View.VISIBLE
+        }
+
+    }
+    companion object {
+        const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val inputEditText  = findViewById<EditText>(R.id.inputEditText)
+        outState.putString(PRODUCT_AMOUNT,  inputEditText.toString())
+    }
+
+
+
+
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,53 @@
 package com.example.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val shareButton = findViewById<TextView>(R.id.share_button)
+        val writeToSupportButton = findViewById<TextView>(R.id.writeToSupport_button)
+        val userAgreement_button = findViewById<TextView>(R.id.userAgreement_button)
+        val backButton = findViewById<ImageView>(R.id.back_button)
+
+        shareButton.setOnClickListener {
+            val message = getString(R.string.link_to_yandexPracticum)
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, message)
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+        writeToSupportButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // only email apps should handle this
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_address)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_1))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.subject_2))
+            }
+            startActivity(shareIntent)
+        }
+        userAgreement_button.setOnClickListener {
+            val webpage: Uri = Uri.parse(getString(R.string.user_agriment))
+            val openWeb = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(openWeb)
+        }
+        backButton.setOnClickListener{
+            finish()
+        }
+
+
+
     }
+
 }
