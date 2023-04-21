939e1d5f5173cc764f5ae80472fcf3b513a1db68
MajoritySky2496 <gzag2496@gmail.com>
2023-04-21 21:26:57 +0500

    Коммит 15.7 1. Реализовываю слой Domain

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -8,7 +8,7 @@ import kotlinx.android.parcel.Parcelize
 data class Track(
     val trackId: String,
     val artistName: String,
-    val trackName: String?,
+    val trackName: String,
     val releaseDate: String?,
     val primaryGenreName: String,
     val country: String,
