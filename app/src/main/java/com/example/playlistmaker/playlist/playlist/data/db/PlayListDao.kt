a89a5d37b8c9ce422bc0d52703853762187ed0c7
MajoritySky2496 <gzag2496@gmail.com>
2023-07-27 11:52:14 +0500

    Коммит 22.0 1.Создал базу данных для Плейлиста 2.Создал DAO интерфейс для плейлиста

---
+++ app/src/main/java/com/example/playlistmaker/playlist/playlist/data/db/PlayListDao.kt
@@ -0,0 +1,29 @@
+package com.example.playlistmaker.playlist.playlist.data.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.playlistmaker.playlist.mediateca.data.db.TrackEntity
+
+@Dao
+interface PlayListDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPlayList(playList: PlayListEntity)
+
+    @Delete
+    suspend fun deletePlayList(playList: PlayListEntity)
+
+    @Update(entity = PlayListEntity::class, onConflict = OnConflictStrategy.REPLACE)
+    fun updateMovie(movieEntity: PlayListEntity)
+
+    @Query("SELECT * FROM track_table")
+    suspend fun getPlayLists(): List<PlayListEntity>
+
+    @Query("SELECT *  FROM playlist_table  WHERE playListId = :playListId")
+    suspend fun getPlayList(playListId:Long):PlayListEntity
+
+
+}
---
+++ app/src/main/java/com/example/playlistmaker/playlist/playlist/data/db/PlayListEntity.kt
@@ -0,0 +1,14 @@
+package com.example.playlistmaker.playlist.playlist.data.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "playlist_table")
+data class PlayListEntity(
+    @PrimaryKey(autoGenerate = true)
+    val playListId:Long,
+    val name:String,
+    val description:String,
+    val idTracks:String,
+    val numberTracks:Int
+)
